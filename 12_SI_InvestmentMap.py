1a04d51bf25a1bdaff10bf6a18fa76b43380769a
NarasetS <80676034+NarasetS@users.noreply.github.com>
2024-09-17 09:36:14 +0700

    DD

--- 12_SI_InvestmentMap.py
+++ 12_SI_InvestmentMap.py
@@ -8,7 +8,7 @@ from shapely import wkt
 import numpy as np
 
 ###################### Config ################################################################################################
-coarsenscale = 1
+coarsenscale = 3
 lccs_resolution = 300 * coarsenscale #m
 areapergrid = (lccs_resolution/1000) ** 2 ## km2
 scenario_SI = 0  ## Include area where SI >= scenario_SI
@@ -170,19 +170,19 @@ print('mwpergrid_solar = ',mwpergrid_solar)
 ###################### Summary ################################################################################################
 
 ####### PDP ############################################################
-quota_wind_total =  6344.8
 quota_wind_R0 = 0
 quota_wind_R1 = 0
-quota_wind_R2 = 6084.7
-quota_wind_R3 =  260.1
+quota_wind_R2 = 6100 #6084.7
+quota_wind_R3 =  300 #260.1
 quota_wind_R4 =  0
+quota_wind_total = quota_wind_R0 +  quota_wind_R1 + quota_wind_R2 + quota_wind_R3 + quota_wind_R4 #6344.8
 
-quota_solar_total =  18129
-quota_solar_R0 =  23.34
-quota_solar_R1 =  3255.36
-quota_solar_R2 =  6072.28
-quota_solar_R3 = 5602.08
-quota_solar_R4 =  3175.94
+quota_solar_R0 =  50 #23.34
+quota_solar_R1 =  3300 #3255.36
+quota_solar_R2 =  6100 #6072.28
+quota_solar_R3 = 5650 #5602.08
+quota_solar_R4 =  3200 #3175.94
+quota_solar_total =  quota_solar_R0 + quota_solar_R1 + quota_solar_R2 + quota_solar_R3 + quota_solar_R4 #18129
 
 # quota_biomass_total = 710.65
 # quota_biomass_R0 = 0
@@ -204,6 +204,12 @@ quota_solar_R4 =  3175.94
 # quota_msw_R2 =  233.65
 # quota_msw_R3 =   104.33
 # quota_msw_R4 =  91
+
+
+print("quota_solar_total = ", quota_solar_total)
+print("quota_wind_total = ", quota_wind_total)
+
+
 ####### PDP ############################################################
 
 ######################## model #####################################################
@@ -247,10 +253,10 @@ constr_maxcap_wind = m.add_constraints(
     cap_wind <= (built_wind * xr_ref['AVA_Wind'] * mwperkm2_wind)
     ,name = 'constr_maxcap_wind'
 )
-constr_mincap_wind = m.add_constraints(
-    cap_wind >= (built_wind * 1)
-    ,name = 'constr_mincap_wind'
-)
+# constr_mincap_wind = m.add_constraints(
+#     cap_wind >= (built_wind * 1)
+#     ,name = 'constr_mincap_wind'
+# )
 constr_builtarea_wind = m.add_constraints(
     built_wind <= (xr_ref['AVA_Wind'] * 10000)
     ,name = 'constr_builtarea_wind'
@@ -260,10 +266,10 @@ constr_maxcap_solar = m.add_constraints(
     cap_solar <= (built_solar * xr_ref['AVA_Solar'] * mwperkm2_solar)
     ,name = 'constr_maxcap_solar'
 )
-constr_mincap_solar = m.add_constraints(
-    cap_solar >= (built_solar * 1)
-    ,name = 'constr_mincap_solar'
-)
+# constr_mincap_solar = m.add_constraints(
+#     cap_solar >= (built_solar * 1)
+#     ,name = 'constr_mincap_solar'
+# )
 constr_builtarea_solar = m.add_constraints(
     built_solar <= (xr_ref['AVA_Solar'] * 10000)
     ,name = 'constr_builtarea_solar'
@@ -314,38 +320,38 @@ constr_builtarea_solar = m.add_constraints(
 constr_quota_wind = m.add_constraints((cap_wind.sum()) == quota_wind_total , name='constr_quota_wind')
 
 constr_quota_wind_r0 = m.add_constraints(lhs = (cap_wind).where((xr_ref['region'] == 'R0'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_wind_R0, name='constr_quota_wind_r0')
+                                             , sign = '==' , rhs = quota_wind_R0, name='constr_quota_wind_r0')
 
 constr_quota_wind_r1 = m.add_constraints(lhs = (cap_wind).where((xr_ref['region'] == 'R1'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_wind_R1, name='constr_quota_wind_r1')
+                                             , sign = '==' , rhs = quota_wind_R1, name='constr_quota_wind_r1')
 
 constr_quota_wind_r2 = m.add_constraints(lhs = (cap_wind).where((xr_ref['region'] == 'R2'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_wind_R2, name='constr_quota_wind_r2')
+                                             , sign = '==' , rhs = quota_wind_R2, name='constr_quota_wind_r2')
 
 constr_quota_wind_r3 = m.add_constraints(lhs = (cap_wind).where((xr_ref['region'] == 'R3'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_wind_R3, name='constr_quota_wind_r3')
+                                             , sign = '==' , rhs = quota_wind_R3, name='constr_quota_wind_r3')
 
 constr_quota_wind_r4 = m.add_constraints(lhs = (cap_wind).where((xr_ref['region'] == 'R4'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_wind_R4, name='constr_quota_wind_r4')
+                                             , sign = '==' , rhs = quota_wind_R4, name='constr_quota_wind_r4')
 
 ############################################################################################################################################################
 
 constr_quota_solar = m.add_constraints((cap_solar.sum()) == quota_solar_total, name='constr_quota_solar')
 
 constr_quota_solar_r0 = m.add_constraints(lhs = (cap_solar).where((xr_ref['region'] == 'R0'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_solar_R0, name='constr_quota_solar_r0')
+                                             , sign = '==' , rhs = quota_solar_R0, name='constr_quota_solar_r0')
 
 constr_quota_solar_r1 = m.add_constraints(lhs = (cap_solar).where((xr_ref['region'] == 'R1'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_solar_R1, name='constr_quota_solar_r1')
+                                             , sign = '==' , rhs = quota_solar_R1, name='constr_quota_solar_r1')
 
 constr_quota_solar_r2 = m.add_constraints(lhs = (cap_solar).where((xr_ref['region'] == 'R2'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_solar_R2, name='constr_quota_solar_r2')
+                                             , sign = '==' , rhs = quota_solar_R2, name='constr_quota_solar_r2')
 
 constr_quota_solar_r3 = m.add_constraints(lhs = (cap_solar).where((xr_ref['region'] == 'R3'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_solar_R3, name='constr_quota_solar_r3')
+                                             , sign = '==' , rhs = quota_solar_R3, name='constr_quota_solar_r3')
 
 constr_quota_solar_r4 = m.add_constraints(lhs = (cap_solar).where((xr_ref['region'] == 'R4'),drop=True).sum()
-                                             , sign = '>=' , rhs = quota_solar_R4, name='constr_quota_solar_r4')
+                                             , sign = '==' , rhs = quota_solar_R4, name='constr_quota_solar_r4')
 
 ###########################################################################################################################################################
 
@@ -407,7 +413,7 @@ constr_quota_solar_r4 = m.add_constraints(lhs = (cap_solar).where((xr_ref['regio
 ##########################################################################################################################################################
 
 ###################### Objective function ################################################################################################
-obj = (-1000) * (
+obj = (-10000) * (
 
     ( xr_ref['SI_Wind'] * (cap_wind  / mwpergrid_wind))
     +
@@ -435,8 +441,8 @@ m.add_objective(obj)
 ###################### Solver ################################################################################################
 print("presolve = ",m)
 m.solve(solver_name='highs',
-        mip_abs_gap = 0.03,
-        mip_rel_gap = 0.03,
+        # mip_abs_gap = 0.03,
+        # mip_rel_gap = 0.03,
         )
 
 print('aftersolve = ',m)
@@ -444,12 +450,12 @@ solution = m.solution
 solution = solution.fillna(0)
 print(solution)
 
-xr_ref['cap_wind'] = np.round(solution['cap_wind'] * solution['built_wind'],1)
-xr_ref['cap_solar'] = np.round(solution['cap_solar'] * solution['built_solar'],1)
-# xr_ref['cap_biomass'] = np.round(solution['cap_biomass'] * solution['built_biomass'],1)
-# xr_ref['cap_bgec'] = np.round(solution['cap_bgec'] * solution['built_bgec'],1)
-# xr_ref['cap_msw'] = np.round(solution['cap_msw'] * solution['built_msw'],1)
-# xr_ref['cap_solar'] = np.round(solution['cap_solar'] * solution['built_solar'],1)
+xr_ref['cap_wind'] = np.round(solution['cap_wind'] * solution['built_wind'],4)
+xr_ref['cap_solar'] = np.round(solution['cap_solar'] * solution['built_solar'],4)
+# xr_ref['cap_biomass'] = np.round(solution['cap_biomass'] * solution['built_biomass'],4)
+# xr_ref['cap_bgec'] = np.round(solution['cap_bgec'] * solution['built_bgec'],4)
+# xr_ref['cap_msw'] = np.round(solution['cap_msw'] * solution['built_msw'],4
+# xr_ref['cap_solar'] = np.round(solution['cap_solar'] * solution['built_solar'],4)
 
 
 print("cap_wind = ",xr_ref['cap_wind'].sum())
